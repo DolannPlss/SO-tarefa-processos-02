8a50ee4346034c98dbec24ee8001d05d0007a377
marco <marco@DESKTOP-KN07FDS>
2020-09-06 18:52:16 -0300

    Segundo Commit

---
+++ AulaProcesosEx02/src/controller/TaskController.java
@@ -0,0 +1,108 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class TaskController {
+	public static String os() {
+		String os = System.getProperty("os.name");
+		return os;
+	}
+	public void readProcess(String process) {
+		String os = TaskController.os();
+		if (os.contains("Windows")) {
+			try {
+				Process p = Runtime.getRuntime().exec(process);
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					System.out.println(linha);
+					linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else {
+			try {
+				Process p = Runtime.getRuntime().exec(process);
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					System.out.println(linha);
+					linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public void killProcessid (int id) {
+		String os = TaskController.os();
+		if (os.contains("Windows")) {	
+			String 	cmdPid = "taskkill /PID";
+			int pid=(id);
+			StringBuffer buffer = new StringBuffer();
+			buffer.append(cmdPid);
+			buffer.append(" ");
+			buffer.append(pid);
+			try {
+				Runtime.getRuntime().exec(buffer.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else{
+			String 	cmdPid = "taskkill /PID";
+			int pid=(id);
+			StringBuffer buffer = new StringBuffer();
+			buffer.append(cmdPid);
+			buffer.append(" ");
+			buffer.append(pid);
+			try {
+				Runtime.getRuntime().exec(buffer.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+			
+	}
+	public void killProcessNM (String Nome) {
+		String os = TaskController.os();
+		if (os.contains("Windows")) {
+			String 	cmdNome = "taskkill /IM";
+			StringBuffer buffer = new StringBuffer();
+			buffer.append(cmdNome);
+			buffer.append(" ");
+			buffer.append(Nome);
+			try {
+				Runtime.getRuntime().exec(buffer.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else{
+			String 	cmdNome = "taskkill /IM";
+			StringBuffer buffer = new StringBuffer();
+			buffer.append(cmdNome);
+			buffer.append(" ");
+			buffer.append(Nome);
+			try {
+				Runtime.getRuntime().exec(buffer.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
+
---
+++ AulaProcesosEx02/src/view/Main.java
@@ -0,0 +1,40 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.TaskController;
+
+
+public class Main {
+	public static void main(String[] args) {
+		TaskController TaController = new TaskController();
+		
+		String os = TaskController.os();
+		System.out.println(os);
+		
+		int opc = 0;
+		while (opc!=9){
+			opc=Integer.parseInt(JOptionPane.showInputDialog("insira comando\n 1-task list\n 2-taskkill por id\n 9-taskkill por nome\n "));
+			switch (opc) {
+			
+			case 1:
+				String process = "tasklist /fo table";
+				TaController.readProcess(process);
+				break;
+			case 2:
+				int id =Integer.parseInt(JOptionPane.showInputDialog("insira id do processo a ser finalizado"));
+				TaController.killProcessid(id);
+				break;
+			case 3:
+				String Nome =(JOptionPane.showInputDialog("insira id do processo a ser finalizado"));
+				TaController.killProcessNM(Nome);
+				break;
+			default: System.out.println("opc invalida");
+				break;
+			case 9:System.out.println("operação finalizada");
+				break;
+			}
+	
+		}
+	}
+}
